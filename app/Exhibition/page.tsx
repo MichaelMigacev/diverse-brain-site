9ad20d48b5befdfadd49d32e7dac748abfbba689
Michael Migacev <micha.migacev@googlemail.com>
2025-04-02 00:05:15 +0200

    removed logobar

--- app/Exhibition/page.tsx
+++ app/Exhibition/page.tsx
@@ -5,54 +5,6 @@ import { useState } from "react";
 export default function ExhibitionPage() {
     return (
         <div>
-            <div className="w-full bg-white h-32 flex items-center">
-                {/* Container for Logos */}
-                <div className="flex justify-evenly items-center w-full max-w-7xl mx-auto px-4">
-                    <Image
-                        src="/diverse-brain-site/logos/FU_Logo.svg"
-                        alt="Logo 1"
-                        width={120}
-                        height={120}
-                        className="object-contain h-16 w-auto"
-                    />
-                    <Image
-                        src="/diverse-brain-site/logos/HU_Logo.svg"
-                        alt="Logo 1"
-                        width={120}
-                        height={120}
-                        className="object-contain h-16 w-auto"
-                    />
-                    <Image
-                        src="/diverse-brain-site/logos/TU_Logo.svg"
-                        alt="Logo 1"
-                        width={120}
-                        height={120}
-                        className="object-contain h-16 w-auto"
-                    />
-                    <Image
-                        src="/diverse-brain-site/logos/Char_Logo.svg"
-                        alt="Logo 1"
-                        width={120}
-                        height={120}
-                        className="object-contain h-16 w-auto"
-                    />
-                    <Image
-                        src="/diverse-brain-site/logos/BUA_Logo_hoch_2_RGB_24.svg"
-                        alt="Logo 2"
-                        width={120}
-                        height={120}
-                        className="object-contain h-24 w-auto"
-                    />
-
-                    <Image
-                        src="/diverse-brain-site/logos/CCNB-blau-s.jpg"
-                        alt="Logo 3"
-                        width={120}
-                        height={120}
-                        className="object-contain h-18 w-auto"
-                    />
-                </div>
-            </div>
             <div className="py-16 px-4 sm:px-6 lg:px-8 bg-white">
                 <div className="text-center mb-12">
                     <Image
