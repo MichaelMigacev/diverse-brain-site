6bdba5d583c33e0d3d3a02387a6ea2d02c58d8ed
Michael Migacev <micha.migacev@googlemail.com>
2025-04-08 16:28:08 +0200

    added new images again

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -12,7 +12,7 @@ export default function TeachingPage() {
                     {/* Image + Name */}
                     <div className="flex flex-col gap-4 px-16 sm:w-1/3">
                         <Image
-                            src="/people/Micha_test.jpeg"
+                            src="/people/Micha-1.JPG"
                             alt="Michael Migacev"
                             width={1000}
                             height={1000}
