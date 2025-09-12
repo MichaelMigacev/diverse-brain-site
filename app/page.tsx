bd6da23b780db4d1a868fdd94f4b15d8461ab9bd
Michael Migacev <micha.migacev@googlemail.com>
2025-09-12 16:10:01 +0200

    changed path

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,11 @@
-import { redirect } from "next/navigation";
 import Slideshow from "../components/Slideshow";
 
 const images = Array.from(
     { length: 34 },
-    (_, i) => `/exhibition/event/img_${i + 1}.jpg`
+    (_, i) => `/exhibition/event/img${i + 1}.jpg`
 );
 
 export default function Home() {
-    if (
-        typeof window !== "undefined" &&
-        window.location.pathname !== "/Exhibition"
-    ) {
-        redirect("/Exhibition");
-    }
     return (
         <div className="pt-24">
             <h1 className="text-4xl font-bold text-center mb-8">
@@ -20,5 +13,5 @@ export default function Home() {
             </h1>
             <Slideshow images={images} />
         </div>
-    ); // Prevents an empty render
+    );
 }
